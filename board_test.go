2b24e79abd8d8f9f68819da74452db3f5cce4409
Ben Trevor <btrevor@8thlight.com>
2014-12-13 13:15:43 -0600

    Rewrite tests

--- board_test.go
+++ board_test.go
@@ -1,115 +1,108 @@
 package checkers_test
 
 import (
-	// . "github.com/bentrevor/checkers"
+	. "github.com/bentrevor/checkers"
 	// "fmt"
 	"testing"
 )
 
-func TestBoard(t *testing.T) {
-	assert(t, true, "\nassertion\n")
+var board = NewGameBoard()
+
+func TestBoard_StartsWith24Pieces(t *testing.T) {
+	assertEquals(t, 24, len(board.Pieces))
 }
 
-// var _ = Describe("a board", func() {
-// 	var board = NewGameBoard()
-// 	fmt.Println()
+func TestBoard_GetsThePieceAtASpace(t *testing.T) {
+	assertEquals(t, 24, len(board.Pieces))
+	whiteSpaces := []Space{
+		Space{File: "a", Rank: 1},
+		Space{File: "c", Rank: 1},
+		Space{File: "e", Rank: 1},
+		Space{File: "g", Rank: 1},
+		Space{File: "b", Rank: 2},
+		Space{File: "d", Rank: 2},
+		Space{File: "f", Rank: 2},
+		Space{File: "h", Rank: 2},
+		Space{File: "a", Rank: 3},
+		Space{File: "c", Rank: 3},
+		Space{File: "e", Rank: 3},
+		Space{File: "g", Rank: 3},
+	}
 
-// 	It("has pieces", func() {
-// 		Expect(len(board.Pieces)).To(Equal(24))
-// 	})
+	blackSpaces := []Space{
+		Space{File: "b", Rank: 6},
+		Space{File: "d", Rank: 6},
+		Space{File: "f", Rank: 6},
+		Space{File: "h", Rank: 6},
+		Space{File: "a", Rank: 7},
+		Space{File: "c", Rank: 7},
+		Space{File: "e", Rank: 7},
+		Space{File: "g", Rank: 7},
+		Space{File: "b", Rank: 8},
+		Space{File: "d", Rank: 8},
+		Space{File: "f", Rank: 8},
+		Space{File: "h", Rank: 8},
+	}
 
-// 	It("gets the piece at a space", func() {
-// 		// board.ConsolePrint()
+	for _, space := range whiteSpaces {
+		assertEquals(t, "white", board.GetPieceAt(space).Color)
+	}
 
-// 		whiteSpaces := []Space{
-// 			Space{File: "a", Rank: 1},
-// 			Space{File: "c", Rank: 1},
-// 			Space{File: "e", Rank: 1},
-// 			Space{File: "g", Rank: 1},
-// 			Space{File: "b", Rank: 2},
-// 			Space{File: "d", Rank: 2},
-// 			Space{File: "f", Rank: 2},
-// 			Space{File: "h", Rank: 2},
-// 			Space{File: "a", Rank: 3},
-// 			Space{File: "c", Rank: 3},
-// 			Space{File: "e", Rank: 3},
-// 			Space{File: "g", Rank: 3},
-// 		}
+	for _, space := range blackSpaces {
+		assertEquals(t, "black", board.GetPieceAt(space).Color)
+	}
+}
 
-// 		blackSpaces := []Space{
-// 			Space{File: "b", Rank: 6},
-// 			Space{File: "d", Rank: 6},
-// 			Space{File: "f", Rank: 6},
-// 			Space{File: "h", Rank: 6},
-// 			Space{File: "a", Rank: 7},
-// 			Space{File: "c", Rank: 7},
-// 			Space{File: "e", Rank: 7},
-// 			Space{File: "g", Rank: 7},
-// 			Space{File: "b", Rank: 8},
-// 			Space{File: "d", Rank: 8},
-// 			Space{File: "f", Rank: 8},
-// 			Space{File: "h", Rank: 8},
-// 		}
 
-// 		for _, space := range whiteSpaces {
-// 			Expect(board.GetPieceAt(space).Color).To(Equal("white"))
-// 		}
+func TestBoard_CanPlaceAPiece(t *testing.T) {
+	emptySpace := Space{File: "e", Rank: 5}
+	occupiedSpace := Space{File: "e", Rank: 7}
 
-// 		for _, space := range blackSpaces {
-// 			Expect(board.GetPieceAt(space).Color).To(Equal("black"))
-// 		}
-// 	})
+	piece1 := Piece{Color: "white", Space: emptySpace}
+	piece2 := Piece{Color: "white", Space: occupiedSpace}
 
-// 	It("can place a piece", func() {
-// 		emptySpace := Space{File: "e", Rank: 5}
-// 		occupiedSpace := Space{File: "e", Rank: 7}
+	_, createdPieceAtE5 := board.PlacePiece(piece1)
+	_, createdPieceAtE7 := board.PlacePiece(piece2)
 
-// 		piece1 := Piece{Color: "white", Space: emptySpace}
-// 		piece2 := Piece{Color: "white", Space: occupiedSpace}
+	assert(t, createdPieceAtE5, "should have placed piece at e5")
+	assert(t, !createdPieceAtE7, "should not have placed piece at e5")
+}
 
-// 		_, createdPieceAtE5 := board.PlacePiece(piece1)
-// 		_, createdPieceAtE7 := board.PlacePiece(piece2)
+func TestBoard_KnowsWhereAPieceCanMove(t *testing.T) {
+	whitePiece := board.GetPieceAt(Space{File: "g", Rank: 3})
+	blackPiece := board.GetPieceAt(Space{File: "h", Rank: 6})
 
-// 		Expect(createdPieceAtE5).To(BeTrue())
-// 		Expect(createdPieceAtE7).To(BeFalse())
-// 	})
+	whiteMove1 := Space{File: "f", Rank: 4}
+	whiteMove2 := Space{File: "h", Rank: 4}
+	blackMove := Space{File: "g", Rank: 5}
 
-// 	It("knows where a piece can move", func() {
-// 		whitePiece := board.GetPieceAt(Space{File: "g", Rank: 3})
-// 		blackPiece := board.GetPieceAt(Space{File: "h", Rank: 6})
+	assertEquals(t, []Space{whiteMove1, whiteMove2}, board.MovesForPiece(whitePiece))
+	assertEquals(t, []Space{blackMove}, board.MovesForPiece(blackPiece))
+}
 
-// 		whiteMove1 := Space{File: "f", Rank: 4}
-// 		whiteMove2 := Space{File: "h", Rank: 4}
-// 		blackMove := Space{File: "g", Rank: 5}
+func TestBoard_KnowsTheSpaceForAnIndex(t *testing.T) {
+	assertEquals(t, Space{File: "a", Rank: 1}, SpaceFor(0))
+	assertEquals(t, Space{File: "b", Rank: 1}, SpaceFor(1))
+	assertEquals(t, Space{File: "h", Rank: 1}, SpaceFor(7))
+	assertEquals(t, Space{File: "a", Rank: 2}, SpaceFor(8))
+	assertEquals(t, Space{File: "b", Rank: 2}, SpaceFor(15))
+	assertEquals(t, Space{File: "h", Rank: 8}, SpaceFor(63))
+}
 
-// 		Expect(board.MovesForPiece(whitePiece)).To(Equal([]Space{whiteMove1, whiteMove2}))
-// 		Expect(board.MovesForPiece(blackPiece)).To(Equal([]Space{blackMove}))
-// 	})
+func TestBoard_KnowsWhereAPieceCanJump(t *testing.T) {
+	d6 := board.GetPieceAt(Space{File: "d", Rank: 6})
+	f6 := board.GetPieceAt(Space{File: "f", Rank: 6})
+	emptySpace := Space{File: "e", Rank: 5}
+	whitePiece := Piece{Color: "white", Space: emptySpace}
 
-// 	It("knows the space for an index", func() {
-// 		Expect(SpaceFor(0)).To(Equal(Space{File: "a", Rank: 1}))
-// 		Expect(SpaceFor(1)).To(Equal(Space{File: "b", Rank: 1}))
-// 		Expect(SpaceFor(7)).To(Equal(Space{File: "h", Rank: 1}))
-// 		Expect(SpaceFor(8)).To(Equal(Space{File: "a", Rank: 2}))
-// 		Expect(SpaceFor(15)).To(Equal(Space{File: "b", Rank: 2}))
-// 		Expect(SpaceFor(63)).To(Equal(Space{File: "h", Rank: 8}))
-// 	})
+	board.PlacePiece(whitePiece)
 
-// 	It("knows where a piece can jump", func() {
-// 		d6 := board.GetPieceAt(Space{File: "d", Rank: 6})
-// 		f6 := board.GetPieceAt(Space{File: "f", Rank: 6})
-// 		emptySpace := Space{File: "e", Rank: 5}
-// 		whitePiece := Piece{Color: "white", Space: emptySpace}
+	captureMoveForD6 := Space{File: "f", Rank: 4}
+	captureMoveForF6 := Space{File: "d", Rank: 4}
 
-// 		board.PlacePiece(whitePiece)
+	d6Moves := board.MovesForPiece(d6)
+	f6Moves := board.MovesForPiece(f6)
 
-// 		captureMoveForD6 := Space{File: "f", Rank: 4}
-// 		captureMoveForF6 := Space{File: "d", Rank: 4}
-
-// 		d6Moves := board.MovesForPiece(d6)
-// 		f6Moves := board.MovesForPiece(f6)
-
-// 		Expect(Includes(d6Moves, captureMoveForD6)).To(BeTrue())
-// 		Expect(Includes(f6Moves, captureMoveForF6)).To(BeTrue())
-// 	})
-// })
+	assert(t, Includes(d6Moves, captureMoveForD6), "d6 capture move")
+	assert(t, Includes(f6Moves, captureMoveForF6), "f6 capture move")
+}
--- bolt_test.go
+++ bolt_test.go
@@ -8,12 +8,16 @@ import (
 	"testing"
 )
 
+// taken from https://github.com/boltdb/bolt/blob/master/bolt_test.go
+
 // assert fails the test if the condition is false.
 func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		fmt.Printf("\n\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
 		tb.FailNow()
+	} else {
+		fmt.Printf("\033[32m.\033[39m")
 	}
 }
 
@@ -21,16 +25,20 @@ func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 func ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		fmt.Printf("\n\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 		tb.FailNow()
+	} else {
+		fmt.Printf("\033[32m.\033[39m")
 	}
 }
 
 // equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
+func assertEquals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		fmt.Printf("\n\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
 		tb.FailNow()
+	} else {
+		fmt.Printf("\033[32m.\033[39m")
 	}
 }
