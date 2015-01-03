d0e1370a49272272da5f5da25f4302d7cabb834e
Ben Trevor <btrevor@8thlight.com>
2015-01-03 11:51:36 -0600

    pass fen in from command line

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import . "github.com/bentrevor/checkers/src"
+import (
+	"flag"
+
+	. "github.com/bentrevor/checkers/src"
+)
+
+var (
+	flagFen = ""
+)
+
+func init() {
+	flag.StringVar(&flagFen, "fen", flagFen, "initial board state")
+
+	flag.Parse()
+}
 
 func main() {
 	player1 := NewHumanPlayer("white")
 	player2 := NewHumanPlayer("black")
 	game := NewGame(player1, player2, ConsoleOutput{})
+
+	if len(flagFen) > 0 {
+		game.InitFromFen(flagFen)
+	}
+
 	game.Start()
 }
